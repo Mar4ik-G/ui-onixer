40c47e4ebeae2fb9aec2b1aff4c92e05722ee182
Mark <brainocswell@gmail.com>
2025-05-10 21:56:57 +0300

    change import and main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import './App.css'
+import './App.scss'
 
 function App() {
+    return (
+        <div className="app">
 
-  return (
-      <>
-          <div>
-            Hello World!
-          </div>
-      </>
-  )
+            <main className="content">
+                <h1>Welcome to My Website</h1>
+                <p>This is the starting layout for your React site.</p>
+            </main>
+
+        </div>
+    )
 }
 
 export default App
